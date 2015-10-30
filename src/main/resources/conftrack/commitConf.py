5c65c678e851c0dfcaa84952602d06fc820ec33a
Dmitry Ganin <DGanin@yotateam.com>
2015-10-30 19:45:43 +0300

    develop

--- src/main/resources/conftrack/commitConf.py
+++ src/main/resources/conftrack/commitConf.py
@@ -1,19 +1,3 @@
-def getDeploymentGroup():
-    print "======================================================================================="
-    print "START: getDeploymentGroup"
-    # Get our deployed containers
-    for delta in deltas.deltas:
-        delta_op = str(delta.operation)
-        deployed = delta.previous if delta_op == "DESTROY" else delta.deployed
-        container = deployed.container
-        host = container.host
-        depGroup = host.deploymentGroup
-        print "Deployment group for %s is %s" % (host, depGroup)
-    print "depGroup = %s " % depGroup
-    print "END  : getDeploymentGroup"
-    print "======================================================================================="
-    return depGroup
-# End def
 
 def getHosts():
     print "======================================================================================="
@@ -32,48 +16,14 @@ def getHosts():
 # End def
 
 
-def getContainerListFromDeployedApplication():
-    print "START: getContainerListFromDeployedApplication"
-    containers = {}
-    env = deployedApplication.getEnvironment()
-    print "ENV = %s" % env.name
-    members = env.getMembers()
-    for container in members:
-       print "Adding %s" % container.host
-       containers[container.host] = container
-    # End if
-    print "END  : getContainerListFromDeployedApplication"
-    return [containers[ke] for ke in containers.keys()]
-# End def
-    
-
-def update_hosts(containers, context):
-    print "START: update_hosts"
-    deploymentGroup = getDeploymentGroup()
-    print containers
-    for container in containers:
-            host = container.host
-            dGrp = host.getProperty('deploymentGroup')
-            print "Container = %s (%s/%s)"  % (host, dGrp, deploymentGroup)
-            if dGrp == deploymentGroup and host.TrackHost:
-                      context.addStep(steps.os_script(
-                         description = "Commit configuration files from host %s" % host,
-                         order = 99,
-                         target_host = host,
-                         script = "conftrack/commit",
-                         freemarker_context = {"env" : deployedApplication.getEnvironment().getName(),
-                                               "version": deployedApplication.getVersion().getVersion(),
-                                               "host": host,
-                                               "app": deployedApplication.getName()
-                                              }
-                     ))
-            # End if
-    print "END  : update_hosts"
-# End def
 
 def update_hosts1(hosts, context):
     print "START: update_hosts1"
     print hosts
+    if context.getDeployedApplication() is None:
+      app = context.getPreviousDeployedApplication()
+    else:
+       app = context.getDeployedApplication()
     for host in hosts:
             print "Container = %s "  % (host)
             if host.TrackHost:
@@ -82,15 +32,14 @@ def update_hosts1(hosts, context):
                          order = 99,
                          target_host = host,
                          script = "conftrack/commit",
-                         freemarker_context = {"env" : deployedApplication.getEnvironment().getName(),
-                                               "version": deployedApplication.getVersion().getVersion(),
+                         freemarker_context = {"env" : app.getEnvironment().getName(),
+                                               "version": app.getVersion().getVersion(),
                                                "host": host,
-                                               "app": deployedApplication.getName()
+                                               "app": app.getName()
                                               }
                      ))
             # End if
     print "END  : update_hosts1"
 # End def
 
-#update_hosts( getContainerListFromDeployedApplication(), context )
 update_hosts1( getHosts(), context )
